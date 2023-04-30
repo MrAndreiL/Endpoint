a057fbabfd82b57977c448b01ff62985ddcd00ee
Lungu Andrei-Sebastian <47700149+MrAndreiL@users.noreply.github.com>
2023-04-30 20:26:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, json, request
 import uuid
+import sys
+import logging
 from azure.cosmos import CosmosClient
 from azure.core.credentials import AzureKeyCredential
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
 
 # Cosmos DB NOSQL config
+logger = logging.getLogger('azure')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
 DATABASE_NAME = "cosmicworks"
 CONTAINER_NAME = "data"
 client = CosmosClient.from_connection_string("AccountEndpoint=https://endpointcosmos.documents.azure.com:443/;AccountKey=m8LD7A5sWsiAqvfPHkGBkQCepDNebI2iunln8GA8fypznI8aFkgNHCTrr1dSwpoldmVswl3oWO1VACDbv1s84A==;")
@@ -14,7 +21,7 @@ database = client.get_database_client(DATABASE_NAME)
 container = database.get_container_client(CONTAINER_NAME)
 
 # pubsub service
-service = WebPubSubServiceClient(endpoint="https://endpointpubsub.webpubsub.azure.com", hub='hub', credential=AzureKeyCredential('Z4HnWctxx27lFBrkQ+Bcr0UdijAARBq5rNxNdsGQ9bo='))
+service = WebPubSubServiceClient(endpoint="https://endpointpubsub.webpubsub.azure.com", hub='hub', credential=AzureKeyCredential('Z4HnWctxx27lFBrkQ+Bcr0UdijAARBq5rNxNdsGQ9bo='), logging_enable=True)
 
 app = Flask(__name__)
 
@@ -29,7 +36,8 @@ def process_json():
     data = json.loads(request.data)
     data["id"] = str(uuid.uuid4())
     container.create_item(data)
-    service.send_to_all(data, content_type="application/json")
+    res = service.send_to_all(data, content_type="application/json", logging_enable=True)
+    logging.debug(res)
     return data, 201
 
 
