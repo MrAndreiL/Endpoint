9ea96e6f8aaed9839846218c39d9beb8df6e6ec8
MrAndreiL <lunguandrei759@gmail.com>
2023-05-28 19:23:55 +0300

    bus

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, json, request
 import uuid
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
 from azure.cosmos import CosmosClient
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
 app = Flask(__name__)
@@ -12,6 +15,8 @@ database = client.get_database_client(DATABASE_NAME)
 
 container = database.get_container_client(CONTAINER_NAME)
 
+NAMESPACE_CONNECTION_STRING = "Endpoint=sb://endpointbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=xccJjKsTNebVapH1MqfOVI5hX+fJh68WN+ASbBTTm/A="
+QUEUE_NAME = "endpointbusqueue"
 @app.route("/", methods=['GET'])
 def hello():
     return "Hello, World", 200
@@ -21,6 +26,11 @@ def process_json():
     data = json.loads(request.data)
     data["id"] = str(uuid.uuid4())
     container.create_item(data)
+    # Send to bus.
+    client = ServiceBusClient.from_connection_string(NAMESPACE_CONNECTION_STRING)
+    sender = client.get_queue_sender(QUEUE_NAME)
+    single_message = ServiceBusMessage(str(data))
+    sender.send_messages(single_message)
     return data, 201
 
 if __name__ == "__main__":
